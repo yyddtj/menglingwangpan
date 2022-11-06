6319155d46ff7f40a7c07fc753375a452bde7e05
yyddtj <99069047+yyddtj@users.noreply.github.com>
2022-11-06 21:31:18 +0800

    Add files via upload

---
+++ js/filemail.js
@@ -0,0 +1,19 @@
+var filemail = {
+	"file": {
+		"title": "梦灵网盘",
+		"desc": "存储美好时光近在咫尺"
+	},
+	"speed": {
+		"title": "科技风格",
+		"desc": "体验不一样的精彩"
+	},
+	"trans": {
+		"title": "全新“黑科技”",
+		"desc": "支持全网自适应，无障碍体验"
+	},
+	"connect": {
+		"title": "多端互通，极简设计",
+		"desc": "支持多终端极简体验"
+	},
+	"codeurl": "img/qq.png"
+}
